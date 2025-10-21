06c72530215b4099ed82a0460b43425cdcbd740a
samkosal <67755908+samkosal@users.noreply.github.com>
2025-10-21 11:13:39 -0800

    10-24-25: added more pages

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 // Enable static files serving 
 app.use(express.static('public'));
 
+// Allow the app to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+//create an Array to store orders
+const orders = [];
+
 // Define the port number where our server will listen 
 const PORT = 3000;
 
@@ -16,12 +22,47 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     // Send "Helow, World!" as a resonce to the client
     // res.send('<h1> Welcome to Poppa\'s Pizza!</h1>');
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+
+// Define a "contact-us" route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+})
+
+// define a "confirmation" route
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+})
+
+// define a "submit-order" route
+app.get('/admin', (req, res) => {   
+
+    res.send(orders);
+    // res.sendFile(`${import.meta.dirname}/views/admin.html`);
+})
+
+app.post('/submit-order', (req, res) => {
+
+    //create a JSON object to store the data
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings,
+        size: req.body.size,
+        comment: req.body.comment
+    };
+    orders.push(order);
+    console.log(orders);
+
+    // send user to confirmation page
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+})
 
 // start the server and make it listen on the port
 // specified above
 app.listen(PORT, () => {
     console.log(`Sever is running at http://localhost:${PORT}`);
 });
-// 
